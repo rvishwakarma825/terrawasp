a8d8b7c1c4cd717dcf457aadf44c947a66a90ea0
rvishwakarma825 <rvishwakarma825@gmail.com>
2017-08-18 12:26:50 +0530

    Create Credentials.tf

---
+++ Credentials.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+        access_key = "${var.accesskey}"
+        secret_key = "${var.secretkey}"
+        region = "eu-west-1"
+}
+
+variable "accesskey" {}
+variable "secretkey" {}
+
+
+variable "keypair" {
+  default = "rvwaspprod"
+}
+
+variable "kafka1-ami" {
+  default = "ami-785db401"
+}
+variable "kafka2-ami" {
+  default = "ami-785db401"
+}
+variable "kafka3-ami" {
+  default = "ami-785db401"
+}
+
+variable "mongo1-ami" {
+  default = "ami-785db401"
+}
+variable "mongo2-ami" {
+  default = "ami-785db401"
+}
+variable "mongo3-ami" {
+  default = "ami-785db401"
+}
+variable "aws_rdsdbname" {
+  default = "Prod_DB"
+}
+variable "aws_rdsusername" {
+  default = "db_user"
+}
+variable "aws_rdspass" {
+  default = "yourpassword"
+}
